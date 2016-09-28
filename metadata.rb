fc07b5e0c13b926ea03002f2226873b75089c960
daytonpa <daytonpa@gmail.com>
2016-09-28 12:22:37 -0400

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'email_server'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures email_server'
+long_description 'Installs/Configures email_server'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/email_server/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/email_server' if respond_to?(:source_url)
